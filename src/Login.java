8fc9bf1a774365c12ce1b28c6eff7cecddee5321
Fahad Alvi <fahad994@gmail.com>
2023-10-07 20:53:40 -0400

    Added transaction page and enabled login functionality

--- src/Login.java
+++ src/Login.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
+import java.sql.*;
 
 public class Login extends JFrame implements ActionListener{
 	
@@ -101,7 +102,21 @@ public class Login extends JFrame implements ActionListener{
 			cardTextField.setText("");
 			pinTextField.setText("");
 		} else if(ae.getSource() == login) {
-			
+			Conn conn = new Conn();
+			String cardnumber = cardTextField.getText();
+			String pinnumber = pinTextField.getText();
+			String query = "select * from login where cardnumber = '"+cardnumber+"' and pin = '"+pinnumber+"'";
+			try {
+				ResultSet rs = conn.s.executeQuery(query);
+				if(rs.next()) {
+					setVisible(false);
+					new Transactions(pinnumber).setVisible(true);
+				}else {
+					JOptionPane.showMessageDialog(null, "Incorrect card number or pin");
+				}
+			}catch(Exception e) {
+				System.out.println(e);
+			}
 		} else if(ae.getSource() == signup) {
 			setVisible(false);
 			new SignupOne().setVisible(true);
